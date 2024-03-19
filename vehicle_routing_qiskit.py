7c1da74f169ba9fa0fe791098115b9b0e6b36898
arishp <arish.p@gmail.com>
2024-03-19 13:47:08 +0530

    update files

---
+++ vehicle_routing_qiskit.py
@@ -0,0 +1,8 @@
+# formalize an idealized version of the problem and showcase its solution using
+# the quantum approximate optimization approach of Farhi, Goldstone, and Gutmann (2014)
+# The overall workflow we demonstrate comprises:
+# # We generate client locations randomly.
+# # We consider the Euclidean distance, “as the crow flies”, for pair-wise distances or time computations.
+# # compute the actual routes twice. First, by classical solver (IBM CPLEX). Second, hybrid algorithm partly on the QC.
+# # visualization of the results is a simplistic plot.
+
